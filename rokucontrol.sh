103bea5f28951397b818c8220dcad5dda3e1370d
Nio <98155633+k4toh@users.noreply.github.com>
2023-06-03 21:32:40 -0500

    Create rokucontrol.sh
    first commit

---
+++ rokucontrol.sh
@@ -0,0 +1,39 @@
+trollface=1
+while [ $trollface -le 100 ]
+do
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Amazon&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Netflix&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=YouTube&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Netflix&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Matrix&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=AmazonTV&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=YouTube&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/keypress/powerOff'
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Netflix&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/keypress/powerOff'        
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Netflix&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=YouTube&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video' 
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Boomerang&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'                      
+curl -d '' '192.168.x.x:8060/search/browse?keyword=AmazonTV&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+curl -d '' '192.168.x.x:8060/keypress/powerOff' 
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/keypress/powerOff'         curl -d '' '192.168.x.x:8060/keypress/powerOn'   
+((trollface=trollface+1))
+done
+curl -d '' '192.168.x.x:8060/keypress/powerOff'
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/keypress/powerOff'
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/search/browse?keyword=Boomerang&type=tv-show&tmsid=SH010855880000&season=4&provider=amazon%20video'
+while [ $trollface -le 100 ]
+do
+curl -d '' '192.168.x.x:8060/keypress/powerOff'         curl -d '' '192.168.x.x:8060/keypress/powerOn'         
+curl -d '' '192.168.x.x:8060/keypress/powerOff'         curl -d '' '192.168.x.x:8060/keypress/powerOn'
+curl -d '' '192.168.x.x:8060/keypress/powerOn'
+
+((trollface=trollface+1))
+done
+
+echo "hahahaha funny"
